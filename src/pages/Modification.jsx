c0bc8f39e78f493d13de486c91baa974ffc71060
sungyeon <tjddus0731@naver.com>
2025-03-06 11:39:06 +0900

    ✨Feat: 회원 정보 수정 페이지 ui구현

---
+++ src/pages/Modification.jsx
@@ -0,0 +1,8 @@
+import ModificationCard from "../components/modification/modificationCard";
+
+export default function ModificationPage() {
+    return (
+      <ModificationCard />
+    );
+  }
+  
