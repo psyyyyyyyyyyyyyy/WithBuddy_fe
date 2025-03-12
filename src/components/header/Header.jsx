37b5af891cd5366860b668a4635f8bb4085391fe
sungyeon <tjddus0731@naver.com>
2025-03-12 10:25:24 +0900

    빌드 오류

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import styles from "./Header.module.css";
+import styles from "./header.module.css";
 import { FiMenu, FiX } from "react-icons/fi";
 
 export default function Header() {
