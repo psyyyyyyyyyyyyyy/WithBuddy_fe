8c7402ac01626239adc3ec5f01fcc3711fc6dfbf
sungyeon <tjddus0731@naver.com>
2025-03-20 13:49:45 +0900

    인앱 브라우저에서 문제

--- src/router/Routes.jsx
+++ src/router/Routes.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import RootLayout from "../components/layout/RootLayout";
 
 import SignUp from "../pages/SignUp";
